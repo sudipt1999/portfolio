859bd5f1b4c8ae1671ffd972ff2ed60ee1ee2e17
sudipt1999 <sudiptdabral2991999@gmail.com>
2020-05-26 23:12:04 +0530

    Updates

---
+++ precache-manifest.b8ef64f62d238875e28e06eddbd7dc5b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d86f3a2d6d174745ceef1b2950c337",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa8d58bd9d78a64c54f5",
+    "url": "/static/css/main.b5704328.chunk.css"
+  },
+  {
+    "revision": "ca9b6801da261c73b583",
+    "url": "/static/js/2.9d9d5dfb.chunk.js"
+  },
+  {
+    "revision": "aa8d58bd9d78a64c54f5",
+    "url": "/static/js/main.841fb82b.chunk.js"
+  },
+  {
+    "revision": "8450a5e86814222ba734",
+    "url": "/static/js/runtime-main.11efcac6.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  }
+]);
